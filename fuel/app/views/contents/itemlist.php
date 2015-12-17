8127e3d2cf5e98e89edaad8d9278207be373cc4c
ymgn <gonyamagon@gmail.com>
2015-12-17 17:32:46 +0900

    itemlist 調整

--- fuel/app/views/contents/itemlist.php
+++ fuel/app/views/contents/itemlist.php
@@ -12,8 +12,8 @@
                   <?php echo Asset::img('contents/pizza.jpg');?>
                 </div>
                 <div class="pizzaname">ソーセージナラベターノ</div>
-                <div class="price">S￥<span>2000</span> M￥<span>3000</span> L￥<span>5000</div>
-		<div class="zei">(税込)</div>
+								<div class="pricelist"><div class="price">￥<span>2000</span></div><div class="price-sml">S￥<span>2000</span> M￥<span>3000</span> L￥<span>5000</span></div></div>
+	              <div class="zei">(税込)</div>
                 <div class="description">ソーセージをふんだんに並べましたああああああああああああああああああああああああああああ</div>
                 <div class="syousai">
                     <a href="" title="" class="biglink">詳細を見る</a>
@@ -24,8 +24,8 @@
                      <?php echo Asset::img('contents/pizza.jpg');?>
                 </div>
                 <div class="pizzaname">コーラ</div>
-                <div class="price">￥2000 </div>
-		<div class="zei">(税込)</div>
+								<div class="pricelist"><div class="price">￥<span>2000</span></div><div class="price-sml">S￥<span>2000</span> M￥<span>3000</span> L￥<span>5000</span></div></div>
+	              <div class="zei">(税込)</div>
                 <div class="description">ソーセージをふんだんに並べました</div>
                 <div class="syousai">
                     <a href="" title="" class="biglink">詳細を見る</a>
@@ -36,8 +36,8 @@
                   <?php echo Asset::img('contents/pizza.jpg');?>
                 </div>
                 <div class="pizzaname">ソーセージナラベターノ</div>
-                <div class="price">S￥<span>2000</span> M￥<span>3000</span> L￥<span>5000</div>
-		<div class="zei">(税込)</div>
+								<div class="pricelist"><div class="price">￥<span>2000</span></div><div class="price-sml">S￥<span>2000</span> M￥<span>3000</span> L￥<span>5000</span></div></div>
+	              <div class="zei">(税込)</div>
                 <div class="description">ソーセージをふんだんに並べましたああああああああああああああああああああああああああああ</div>
                 <div class="syousai">
                     <a href="" title="" class="biglink">詳細を見る</a>
@@ -48,16 +48,50 @@
                      <?php echo Asset::img('contents/pizza.jpg');?>
                 </div>
                 <div class="pizzaname">コーラ</div>
-                <div class="price">￥<span>9000</span></div>
-		<div class="zei">(税込)</div>
+								<div class="pricelist"><div class="price">￥<span>2000</span></div><div class="price-sml">S￥<span>2000</span> M￥<span>3000</span> L￥<span>5000</span></div></div>
+	              <div class="zei">(税込)</div>
                 <div class="description">ソーセージをふんだんに並べました</div>
                 <div class="syousai">
                     <a href="" title="" class="biglink">詳細を見る</a>
                 </div>
             </div>
-        
+						<div class="listitem">
+								<div class="img">
+										 <?php echo Asset::img('contents/pizza.jpg');?>
+								</div>
+								<div class="pizzaname">コーラ</div>
+								<div class="pricelist"><div class="price">￥<span>2000</span></div><div class="price-sml">S￥<span>2000</span> M￥<span>3000</span> L￥<span>5000</span></div></div>
+								<div class="zei">(税込)</div>
+								<div class="description">ソーセージをふんだんに並べました</div>
+								<div class="syousai">
+										<a href="" title="" class="biglink">詳細を見る</a>
+								</div>
+						</div>
+						<div class="listitem">
+								<div class="img">
+										 <?php echo Asset::img('contents/pizza.jpg');?>
+								</div>
+								<div class="pizzaname">コーラ</div>
+								<div class="pricelist"><div class="price">￥<span>2000</span></div><div class="price-sml">S￥<span>2000</span> M￥<span>3000</span> L￥<span>5000</span></div></div>
+								<div class="zei">(税込)</div>
+								<div class="description">ソーセージをふんだんに並べました</div>
+								<div class="syousai">
+										<a href="" title="" class="biglink">詳細を見る</a>
+								</div>
+						</div>
+						<div class="listitem">
+								<div class="img">
+										 <?php echo Asset::img('contents/pizza.jpg');?>
+								</div>
+								<div class="pizzaname">コーラ</div>
+								<div class="pricelist"><div class="price">￥<span>2000</span></div><div class="price-sml">S￥<span>2000</span> M￥<span>3000</span> L￥<span>5000</span></div></div>
+								<div class="zei">(税込)</div>
+								<div class="description">ソーセージをふんだんに並べました</div>
+								<div class="syousai">
+										<a href="" title="" class="biglink">詳細を見る</a>
+								</div>
+						</div>
+					</div>
 	<div class="toppage">
 		<a href="" title="" class="biglink">TOPページに戻る</a>
 	</div>
-</div>
-
